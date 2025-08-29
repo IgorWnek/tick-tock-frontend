c99e5c960aa9f70e657d84175cda3c0eeea341d4
Igor Wnek <igowne@gmail.com>
2025-08-29 12:45:50 +0200

    feat(auth): implement profile management mutations and queries

--- src/api/actions/index.ts
+++ src/api/actions/index.ts
@@ -1,8 +1,10 @@
 import { authMutations } from './auth/auth.mutations';
+import { profileMutations } from './auth/profile.mutations';
 import { timeLogsMutations } from './timeLogs/timeLogs.mutations';
 
 export const mutations = {
   ...authMutations,
+  ...profileMutations,
   ...timeLogsMutations,
   // API_COLLECTION_MUTATIONS
 } as const;
